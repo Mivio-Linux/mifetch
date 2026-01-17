0a09d22599e3d64aa7246d78bfef58880bd917bd
miviodev <miviodev@gmail.com>
2026-01-17 19:19:09 -0300

    add colors

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # MiFetch
-Mini fetch written in ~27 strokes of code. Made just for fun
-
+## Mini fetch written in ~27 lines of code. Made just for fun
+if you want to make custom ASCII, then you need open/create `/etc/mifetch/ascii.txt` and make ascii.
+To colorize in start every line add:
+`$1` - red
+`$2` - green
+`$3` - blue
+`$4` - yellow
+`$5` - purple
+to add custom colors, you can add to `colors` (in 3 line) your colors.
 # build
 ```bash
 git clone https://gitea.com/miviodev/mifetch.git
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
+use colored::Colorize;
 fn main() {
-    let PATH_TO_ASCII: String = String::from("ascii.txt");
+    let colors = ["red", "green", "blue", "yellow", "purple"];
+    let path_to_ascii: String = String::from("/etc/mifetch/ascii.txt");
     let mut sys = sysinfo::System::new_all();
     sys.refresh_all();
-    // получаю данные
     let os = sysinfo::System::name().unwrap_or("Unknown System".to_string());
     let ram = round::round(sys.total_memory() as f64/1073741824.0,2 );
     let swap = round::round(sys.total_swap() as f64/1073741824.0, 2);
     let user_ram = round::round(sys.used_memory() as f64/1073741824.0,2);
     let host_name = sysinfo::System::host_name().unwrap_or("Unknown Host".to_string());
     let cpu = sys.cpus().first().unwrap();
-    // читаю ascii.txt
-    let mut ascii: Result<String, std::io::Error> = std::fs::read_to_string(&PATH_TO_ASCII);
-    ascii = Ok(ascii.unwrap_or_else(|e| {
-        String::from("[ERROR]: can't find ascii.txt")
-    }));
-    // вывожу
-    println!("{}", ascii.unwrap());
-    println!("CPU: {}", cpu.brand());
-    println!("OS: {}", os);
-    println!("RAM: {}GB", ram, );
-    println!("USED RAM: {}GB", user_ram);
-    println!("SWAP: {}GB", swap);
-    println!("HOST: {}", host_name);
+    let ascii = std::fs::read_to_string(path_to_ascii).unwrap_or_else(|e| {
+        eprintln!("[ERROR]: {}", e);
+        "[ERROR]: Can't read ascii.txt. Check /etc/mifetch/ascii.txt file".to_string()
+    });
+    for line in ascii.lines() {
+        if line.starts_with("$"){
+            let c = line.chars().nth(1).unwrap().to_digit(10);
+            let color_name = colors[c.unwrap() as usize -1];
+            let cropped: String = line.chars().skip(2).collect();
+            println!("{}", cropped.color(color_name));
+        }else {
+                println!("{}", line)
+            }
+    }
+    println!("{}: {}", "CPU".bold().red(), cpu.brand());
+    println!("{}: {}","OS".bold().green(), os);
+    println!("{}: {}GB","RAM".bold().blue(), ram, );
+    println!("{}: {}GB","USED RAM".bold().cyan(), user_ram);
+    println!("{}: {}GB","SWAP".bold().purple(), swap);
+    println!("{}: {}", "HOST".bold().white(), host_name);
 
 
 }
